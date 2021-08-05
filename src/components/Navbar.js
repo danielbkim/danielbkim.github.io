dd73c95ec5130879b8e41c7c5948e1067430fbff
Daniel B Kim <danielkim@Daniels-MBP.fios-router.home>
2021-08-05 19:08:05 -0400

    sets up tailwind environment with config files for tailwind and craco

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,18 @@ import React from 'react';
 import '../App.css';
 
 const Navbar = () => {
+  window.onscroll = function() { dynamicProgress() };
+
+  const dynamicProgress = () => {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
   return (
     <div class="header">
-      <nav>Hello!</nav>
+      <nav>Daniel B. Kim</nav>
       <div class="progress-container">
         <div class="progress-bar" id="myBar"></div>
       </div>
