10bf1d15966cad5138d4973e05eb756ddc7ea9cd
Daniel B Kim <danielkim@Daniels-MBP.fios-router.home>
2021-08-11 18:54:01 -0400

    streamlines navbar files

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { NavbarStyles } from './NavbarStyles';
 
 const Navbar = () => {
   return (
-        <NavbarStyles>
-          <div className="name">Daniel B. Kim</div>
-          <div className="nav-links">
-            <ul>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Work</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Hire Now!</a></li>
-            </ul>
-          </div>
-        </NavbarStyles>
+    <nav>
+      <div className="name">Daniel B. Kim</div>
+      <div className="nav-links">
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Hire Now!</a></li>
+        </ul>
+      </div>
+    </nav>
+
   )
 };
 
