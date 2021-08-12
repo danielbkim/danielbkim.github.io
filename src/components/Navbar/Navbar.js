dc6bcedbc362b82d1e415f3ddbbd45f44e57233c
Daniel B Kim <danielkim@Daniels-MBP.fios-router.home>
2021-08-11 21:41:47 -0400

    saves WIP

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { NavbarStyles } from './NavbarStyles';
+import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="name">Daniel B. Kim</div>
+    <NavbarStyles className="nav-container">
+      <div className="logo-container">
+        <img src={ logo } alt="logo"></img>
+        <div className="name">Daniel B. Kim</div>
+      </div>
       <div className="nav-links">
         <ul>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Work</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Hire Now!</a></li>
+          {/* <li><a href="">About</a></li>
+          <li><a href="">Work</a></li>
+          <li><a href="">Blog</a></li>
+          <li><a href="#">Hire Now!</a></li> */}
         </ul>
       </div>
-    </nav>
+    </NavbarStyles>
 
   )
 };
