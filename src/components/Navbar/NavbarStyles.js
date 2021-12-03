216043b001e1aae4037b2ede901e2f5e620b547f
Daniel Kim <bungfoon@gmail.com>
2021-12-03 16:44:20 -0500

    testing iconscout

--- src/components/Navbar/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const NavbarStyles = styled.nav`
-
+  .logo-container {
+    height: 10%;
+  }
 `
--- src/components/Navbar/ProgressBarStyles.js
+++ src/components/Navbar/ProgressBarStyles.js
@@ -4,14 +4,14 @@ export const ProgressBarStyles = styled.div`
   .progress-container {
     display: block;
     width: 100%;
-    height: 8px;
+    height: 4px;
     background: #ccc;
     position: fixed;
   }
 
   .progress-bar {
     width: 0%;
-    height: 8px;
+    height: 4px;
     background: #04AA6D;
   }
 `
