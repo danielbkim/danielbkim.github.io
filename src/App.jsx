c66c61f759bc78a29027721452afc93e860bc45a
Daniel Kim <bungfoon@gmail.com>
2021-12-10 12:57:15 -0500

    styles hamburger menu dropdown to come in and out from the right side of screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 
 import "./app.scss";
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="app">
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <div className="sections">
           <Intro />
           <Projects />
