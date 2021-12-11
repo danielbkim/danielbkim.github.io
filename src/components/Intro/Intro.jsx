c0234e8b14c3db41bdff7c5c162d479fd8cb677e
Daniel Kim <bungfoon@gmail.com>
2021-12-11 18:07:18 -0500

    adds intro mvp styling

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,19 @@ import "./intro.scss";
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                {/* <div className="imgContainer">
+                    maybe an image of yourself
+                </div> */}
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Daniel B. Kim</h1>
+                    <h3>Software Engineer<span></span></h3>
+                </div>
+            </div>
+            {/* maybe add an icon button that takes you to the next div */}
         </div>
     )
 };
