47780a23b2498001b7081b451ef94c6a6f904f71
Daniel Kim <bungfoon@gmail.com>
2021-12-11 18:21:02 -0500

    adds full intro banner mvp using ityped library

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            strings: ["Software Engineer", "Designer", "Woodworker", "Photographer", "Creator"]
+        });
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +25,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi there, I'm</h2>
                     <h1>Daniel B. Kim</h1>
-                    <h3>Software Engineer<span></span></h3>
+                    <h3><span ref={textRef}></span></h3>
                 </div>
             </div>
             {/* maybe add an icon button that takes you to the next div */}
