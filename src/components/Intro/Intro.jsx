679acf5ba0a28cc6a4d264fd766c72d196142f1c
Daniel Kim <bungfoon@gmail.com>
2021-12-11 18:23:46 -0500

    adds under construction for deployment

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -10,7 +10,8 @@ export default function Intro() {
         init(textRef.current, {
             showCursor: true,
             backDelay: 1500,
-            strings: ["Software Engineer", "Designer", "Woodworker", "Photographer", "Creator"]
+            // strings: ["Software Engineer", "Designer", "Woodworker", "Photographer", "Creator"]
+            strings: ["This Portfolio is Under Construction"]
         });
     }, []);
 
